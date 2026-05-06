b249e0f9c3cc928df287d6df35aba4e70360a351
Lakshmikant_Hunachal <kanthunachal@gmail.com>
2026-05-06 17:00:50 +0530

    Time: 8 ms (59.38%) | Memory: 124.7 MB (76.68%) - LeetSync

---
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -0,0 +1,33 @@
+class Solution {
+    public char[][] rotateTheBox(char[][] box) {
+        int m = box.length;
+        int n = box[0].length;
+
+        // Step 1: apply gravity (move stones to right)
+        for (int i = 0; i < m; i++) {
+            int empty = n - 1;
+
+            for (int j = n - 1; j >= 0; j--) {
+                if (box[i][j] == '*') {
+                    empty = j - 1;
+                } else if (box[i][j] == '#') {
+                    char temp = box[i][empty];
+                    box[i][empty] = '#';
+                    if (empty != j) box[i][j] = '.';
+                    empty--;
+                }
+            }
+        }
+
+        // Step 2: rotate 90° clockwise
+        char[][] ans = new char[n][m];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[j][m - i - 1] = box[i][j];
+            }
+        }
+
+        return ans;
+    }
+}
