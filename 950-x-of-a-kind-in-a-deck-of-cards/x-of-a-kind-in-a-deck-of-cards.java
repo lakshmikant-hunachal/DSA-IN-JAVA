d4bf6d2c4beca3e6308d9dfbb541576b3f090dca
Lakshmikant_Hunachal <kanthunachal@gmail.com>
2026-06-18 21:51:32 +0530

    Time: 11 ms (62.62%) | Memory: 47.2 MB (68.21%) - LeetSync

---
+++ 950-x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean hasGroupsSizeX(int[] deck) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Count frequency of each card
+        for (int num : deck) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int gcdValue = 0;
+
+        // Find GCD of all frequencies
+        for (int count : map.values()) {
+            gcdValue = gcd(gcdValue, count);
+        }
+
+        return gcdValue > 1;
+    }
+
+    private int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
