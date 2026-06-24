d8e5fc74682047e325da2fda0bb25fc3e938b80a
Lakshmikant_Hunachal <kanthunachal@gmail.com>
2026-06-24 08:16:08 +0530

    Time: 1 ms (88.65%) | Memory: 42.5 MB (89.01%) - LeetSync

---
+++ 1026-string-without-aaa-or-bbb/string-without-aaa-or-bbb.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String strWithout3a3b(int a, int b) {
+        StringBuilder sb = new StringBuilder();
+        while(a>0 || b>0)
+        {
+            int n=sb.length();
+            if(n>=2 && sb.charAt(n-1)==sb.charAt(n-2))
+            {
+                if(sb.charAt(n-1)=='a')
+                {
+                    sb.append("b");
+                    b--;
+                }
+                else
+                {
+                    sb.append("a");
+                    a--;
+                }
+            }
+            else{
+                if(a>=b && a>0)
+                {
+                    sb.append("a");
+                    a--;
+                }
+                else if(b>0)
+                {
+                    sb.append("b");
+                    b--;
+                }
+                else{
+                    sb.append("a");
+                    a--;
+                }
+            }
+        }
+       return sb.toString();
+    }
+}
