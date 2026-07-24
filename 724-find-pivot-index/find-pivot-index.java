67c33da36cfc9409a8bedd9d34664dcdc69f9ab8
Lakshmikant_Hunachal <kanthunachal@gmail.com>
2026-07-24 14:35:45 +0530

    Time: 1 ms (97.99%) | Memory: 47.4 MB (40.88%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,23 +1,45 @@
+// // class Solution {
+//     public int pivotIndex(int[] nums) {
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             int left=sum(nums,0,i-1);
+//             int right=sum(nums,i+1,nums.length-1);
+//             if(left==right)
+//             {
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+//     private int sum(int arr[],int s,int e)
+//     {
+//         int sum=0;
+//         for(int i=s;i<=e;i++)
+//         {
+//             sum+=arr[i];
+//         }
+//         return sum;
+//     }
+// //}
+// optimal solution
 class Solution {
     public int pivotIndex(int[] nums) {
+        int sum=0;
         for(int i=0;i<nums.length;i++)
         {
-            int left=sum(nums,0,i-1);
-            int right=sum(nums,i+1,nums.length-1);
+            sum+=nums[i];
+        }
+        int left=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            int right = sum - left- nums[i];
             if(left==right)
             {
                 return i;
             }
+            left+=nums[i];
         }
         return -1;
     }
-    private int sum(int arr[],int s,int e)
-    {
-        int sum=0;
-        for(int i=s;i<=e;i++)
-        {
-            sum+=arr[i];
-        }
-        return sum;
-    }
+
 }
